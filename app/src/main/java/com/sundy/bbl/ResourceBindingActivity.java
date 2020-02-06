ccc84283fdbb8767e99013c17db200acc3e30163
sundy <core_it@163.com>
2020-02-07 00:32:36 +0800

    添加测试代码

---
+++ app/src/main/java/com/sundy/bbl/ResourceBindingActivity.java
@@ -0,0 +1,14 @@
+package com.sundy.bbl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResourceBindingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bbl_resource_binding_layout);
+    }
+}
